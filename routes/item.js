c5564496c7a77a120cae60f63a390f6124082396
Chris Rutherford <chris@dittach.com>
2019-04-19 13:31:16 -0400

    completed testing restaurant and item routes. Order Routes Next.

--- routes/item.js
+++ routes/item.js
@@ -2,31 +2,33 @@ const router = require('express').Router();
 
 const { Item } = require('./routeActions');
 
-module.exports = () => {
-  /**
-   * GET - Query
-   */
-  router.get('/', Item.getByQuery);
-  /**
-   * GET - By ID
-   */
-  router.get('/:itemId', Item.getById);
-  /**
-   * POST - create item
-   */
-  router.post('/', Item.create);
-  router.post('/restaurant/:restaurantId/:itemId', Item.addToRestaurant);
-  router.post('/order/:orderId/:itemId', Item.addToOrder);
-  /**
-   * PUT - update item
-   */
-  router.put('/:itemId', Item.updateItem);
-  router.delete('/restaurant/:restaurantId/:itemId', Item.removeFromRestaurant);
-  router.delete('/order/:orderId/:itemId', Item.removeFromOrder);
-  /**
-   * DELETE - delete item
-   */
-  router.delete('/:itemId', Item.deleteItem);
+/**
+ * GET - Query
+ */
+router.get('/', Item.getByQuery);
+/**
+ * GET - By ID
+ */
+router.get('/:itemId', Item.getById);
+/**
+ * POST - create item
+ */
+router.post('/', Item.create);
+//add item to restaurant.
+router.post('/restaurant/:restaurantId/:itemId', Item.addToRestaurant);
+//add item to order.
+router.post('/order/:orderId/:itemId', Item.addToOrder);
+/**
+ * PUT - update item
+ */
+router.put('/:itemId', Item.updateItem);
+//delete item from restaurant. leaves the item to be added to other restaurants.
+router.delete('/restaurant/:restaurantId/:itemId', Item.removeFromRestaurant);
+//delete item from order.
+router.delete('/order/:orderId/:itemId', Item.removeFromOrder);
+/**
+ * DELETE - delete item
+ */
+router.delete('/:itemId', Item.deleteItem);
 
-  return router;
-};
+module.exports = router;
--- routes/routeActions/item.js
+++ routes/routeActions/item.js
@@ -1,101 +1,101 @@
 const Models = require('../../models'),
-  { Items, Restaurants, Orders } = Models;
+	{ Items, Restaurants, Orders } = Models;
 
 const Validators = require('../routeValidators'),
-  { validateCreate } = Validators.Item;
+	{ validateCreate } = Validators.Item;
 
 module.exports = Item = {
-  getByQuery: async (req, res) => {
-    try {
-      const data = await Items.query(req.query);
-      res.json(data);
-    } catch (e) {
-      res.status(500).json(err);
-    }
-  },
-  getById: async (req, res) => {
-    const { itemId } = req.params;
-    try {
-      const item = await Items.findById(itemId);
-      res.json(item);
-    } catch (e) {
-      res.status(500).json(e);
-    }
-  },
-  create: async (req, res) => {
-    const item = req.body;
-    const { errors, isValid } = validateCreate(item);
-    if (!isValid) res.status(400).json(errors);
-    try {
-      const newItem = await new Items(item).save();
-      res.json(newItem);
-    } catch (e) {
-      res.status(500).json(e);
-    }
-  },
-  updateItem: async (req, res) => {
-    const { itemId } = req.params;
-    const updatedItem = req.body;
-    try {
-      const uItem = await Items.findByIdAndUpdate(itemId, updatedItem);
-      res.json(uItem);
-    } catch (e) {
-      res.status(500).json(e);
-    }
-  },
-  deleteItem: async (req, res) => {
-    const { itemId } = req.params;
-    try {
-      const i2d = await Items.deleteOne({ _id: itemId });
-      res.json(i2d);
-    } catch (e) {
-      res.status(500).json(e);
-    }
-  },
-  addToRestaurant: async (req, res) => {
-    const { restaurantId, itemId } = req.params;
-    try {
-      const restaurant = await Restaurants.findById(restaurantId);
-      const item = await Item.findById(itemId);
-      restaurant.items.push(item);
-      const result = await restaurant.save();
-      res.json(result);
-    } catch (e) {
-      res.status(500).json(e);
-    }
-  },
-  addToOrder: async (req, res) => {
-    const { orderId, itemId } = req.params;
-    try {
-      const order = await Orders.findById(orderId);
-      const item = await Items.findById(itemId);
-      order.items.push(item);
-      const result = await order.save();
-      res.json(result);
-    } catch (e) {
-      res.status(500).json(e);
-    }
-  },
-  removeFromRestaurant: async (req, res) => {
-    const { restaurantId, itemId } = req.params;
-    try {
-      const restaurant = await Restaurants.findById(restaurantId);
-      restaurant.items = restaurant.items.filter(x => !x.equals(itemId));
-      const result = await restaurant.save();
-      res.json(result);
-    } catch (e) {
-      res.status(500).json(e);
-    }
-  },
-  removeFromOrder: async (req, res) => {
-    const { orderId, itemId } = req.params;
-    try {
-      const order = await Orders.findById(orderId);
-      order.items = order.items.filter(x => !x.equals(itemId));
-      const result = await order.save();
-      res.json(result);
-    } catch (e) {
-      res.status(500).json(e);
-    }
-  },
+	getByQuery: async (req, res) => {
+		try {
+			const data = await Items.query(req.query);
+			res.json(data);
+		} catch (e) {
+			res.status(500).json(err);
+		}
+	},
+	getById: async (req, res) => {
+		const { itemId } = req.params;
+		try {
+			const item = await Items.findById(itemId);
+			res.json(item);
+		} catch (e) {
+			res.status(500).json(e);
+		}
+	},
+	create: async (req, res) => {
+		const item = req.body;
+		const { errors, isValid } = validateCreate(item);
+		if (!isValid) res.status(400).json(errors);
+		try {
+			const newItem = await new Items(item).save();
+			res.json(newItem);
+		} catch (e) {
+			res.status(500).json(e);
+		}
+	},
+	updateItem: async (req, res) => {
+		const { itemId } = req.params;
+		const updatedItem = req.body;
+		try {
+			const uItem = await Items.findOneAndUpdate({ _id: itemId }, updatedItem);
+			res.json(uItem);
+		} catch (e) {
+			res.status(500).json(e);
+		}
+	},
+	deleteItem: async (req, res) => {
+		const { itemId } = req.params;
+		try {
+			const i2d = await Items.deleteOne({ _id: itemId });
+			res.json(i2d);
+		} catch (e) {
+			res.status(500).json(e);
+		}
+	},
+	addToRestaurant: async (req, res) => {
+		const { restaurantId, itemId } = req.params;
+		try {
+			const restaurant = await Restaurants.findById(restaurantId);
+			const item = await Item.findById(itemId);
+			restaurant.items.push(item);
+			const result = await restaurant.save();
+			res.json(result);
+		} catch (e) {
+			res.status(500).json(e);
+		}
+	},
+	addToOrder: async (req, res) => {
+		const { orderId, itemId } = req.params;
+		try {
+			const order = await Orders.findById(orderId);
+			const item = await Items.findById(itemId);
+			order.items.push(item);
+			const result = await order.save();
+			res.json(result);
+		} catch (e) {
+			res.status(500).json(e);
+		}
+	},
+	removeFromRestaurant: async (req, res) => {
+		const { restaurantId, itemId } = req.params;
+		try {
+			const restaurant = await Restaurants.findById(restaurantId);
+			restaurant.items = restaurant.items.filter(x => !x.equals(itemId));
+			const result = await restaurant.save();
+			res.json(result);
+		} catch (e) {
+			res.status(500).json(e);
+		}
+	},
+	removeFromOrder: async (req, res) => {
+		const { orderId, itemId } = req.params;
+		try {
+			const order = await Orders.findById(orderId);
+			order.items = order.items.filter(x => !x.equals(itemId));
+			const result = await order.save();
+			res.json(result);
+		} catch (e) {
+			res.status(500).json(e);
+		}
+	},
 };
